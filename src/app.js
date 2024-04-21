35ff064cddddb6fa99188136604e341b464f4917
prueba1661 <prueba.henrycommerce@gmail.com>
2024-04-21 02:34:51 -0300

    cors-config

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors');
 
 
 require('./db.js');
 
 const server = express();
-server.use(cors());
+server.use(cors(
+    {
+      origin: ["https://front-videogames-kohl.vercel.app"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true
+    }
+));
 
 server.name = 'API';
 
@@ -18,7 +24,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://front-videogames-kohl.vercel.app');
+  res.header('Access-Control-Allow-Origin');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
